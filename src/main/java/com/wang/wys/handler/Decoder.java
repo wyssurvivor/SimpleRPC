042c1998df344ee8de21e197e452a31ade3d95b4
wyssurvivor <937423966@qq.com>
2017-06-04 16:30:52 +0800

    update Decoder&Encoder

--- src/main/java/com/wang/wys/handler/Decoder.java
+++ src/main/java/com/wang/wys/handler/Decoder.java
@@ -1,7 +1,18 @@
 package com.wang.wys.handler;
 
+import com.wang.wys.model.RPCRequest;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
 /**
  * Created by Ryan on 17/6/4.
  */
-public class Decoder {
+public class Decoder extends ByteToMessageDecoder {
+
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+
+    }
 }
--- src/main/java/com/wang/wys/handler/Encoder.java
+++ src/main/java/com/wang/wys/handler/Encoder.java
@@ -1,7 +1,15 @@
 package com.wang.wys.handler;
 
+import com.wang.wys.model.RPCRequest;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
 /**
  * Created by Ryan on 17/6/4.
  */
-public class Encoder {
+public class Encoder extends MessageToByteEncoder<RPCRequest> {
+    protected void encode(ChannelHandlerContext channelHandlerContext, RPCRequest rpcRequest, ByteBuf byteBuf) throws Exception {
+
+    }
 }
