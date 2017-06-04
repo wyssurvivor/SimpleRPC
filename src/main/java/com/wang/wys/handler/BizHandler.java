2b31d1ce3acfb4ba10263848bc318aaf4e9b9c27
wyssurvivor <937423966@qq.com>
2017-06-04 16:50:29 +0800

    update SimpleServer

--- src/main/java/com/wang/wys/handler/BizHandler.java
+++ src/main/java/com/wang/wys/handler/BizHandler.java
@@ -1,7 +1,14 @@
 package com.wang.wys.handler;
 
+import com.wang.wys.model.RPCRequest;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
 /**
  * Created by Ryan on 17/6/4.
  */
-public class BizHandler {
+public class BizHandler extends SimpleChannelInboundHandler<RPCRequest> {
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, RPCRequest rpcRequest) throws Exception {
+
+    }
 }
--- src/main/java/com/wang/wys/server/SimpleServer.java
+++ src/main/java/com/wang/wys/server/SimpleServer.java
@@ -1,8 +1,54 @@
 package com.wang.wys.server;
 
+import com.wang.wys.handler.BizHandler;
+import com.wang.wys.handler.Decoder;
+import com.wang.wys.handler.Encoder;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
 /**
  * Created by Ryan on 17/6/4.
  */
 public class SimpleServer {
 
+    private int port;
+    public SimpleServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(this.port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline()
+                                    .addLast(new Decoder())
+                                    .addLast(new BizHandler())
+                                    .addLast(new Encoder());
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.bind().sync();
+            System.out.println("server start up");
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = 3494;
+        new SimpleServer(port).start();
+    }
 }
