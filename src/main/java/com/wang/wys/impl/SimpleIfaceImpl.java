381a580c4cfcb4bce70577d2ee38d832f690f94d
wyssurvivor <937423966@qq.com>
2017-06-24 17:35:58 +0800

    add iface

---
+++ src/main/java/com/wang/wys/impl/SimpleIfaceImpl.java
@@ -0,0 +1,12 @@
+package com.wang.wys.impl;
+
+import com.wang.wys.interfaces.SimpleIface;
+
+/**
+ * Created by Ryan on 17/6/24.
+ */
+public class SimpleIfaceImpl implements SimpleIface {
+    public int add(int val1, int val2) {
+        return val1 + val2;
+    }
+}
